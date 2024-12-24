5d8b21471bd8d5fdbd3c90dbc34b0705c97df051
olanrewaju-07 <issabasheet@gmail.com>
2024-12-24 21:54:47 +0100

    main

--- src/Component/Download.jsx
+++ src/Component/Download.jsx
@@ -1,4 +1,4 @@
-import mobileGreenflow from "/assets/images/Mobile-GreenFlow.png"
+
 import playStore from "/assets/icons/get-on-playstore.svg"
 import appleStore from "/assets/icons/Download-on-appstore.svg"
 import { FaCircleCheck } from "react-icons/fa6"
@@ -6,7 +6,7 @@ export default function Download () {
     return (
         <div className="flex items-center justify-center mt-10 mobile:p-4">
         <div className="flex flex-row bg-[#167206] justify-between gap-5 p-6 rounded-xl mobile:flex-col mobile:items-center">
-            <img src={mobileGreenflow} alt="mobile app" className="h-56 mobile:h-56 mobile:w-28 transform -rotate-12" />
+            <img src="/assets/images/mobileImage.png" alt="mobile app" className="h-56 mobile:h-56 mobile:w-28 transform -rotate-12" />
             <div className="flex flex-col items-start gap-4 ml-10">
                 <h2 className="text-white font-semibold text-3xl mobile:text-[23px]">Get complete experience</h2>
                 <p className="text-white text-[13px] w-[75%] mobile:text-[16px] mobile:w-full">Download our mobile app and enjoy convenience like never before.</p>
